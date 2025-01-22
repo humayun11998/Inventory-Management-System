c4db0d8736026a133ce4f706b5e863ce1e0ec90c
Humayun <humayun11998@gmail.com>
2025-01-22 21:18:55 +0500

    Change in sidebar

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -1,146 +1,150 @@
 <!doctype html>
 <html lang="en">
+<head>
 
-    <head>
+    <meta charset="utf-8" />
+    <title>Dashboard | Inventory Management System</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesdesign" name="author" />
+    <!-- Select 2 -->
+    <link href="{{ asset('ims/assets/libs/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css">
+    <!-- end Select 2  -->
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('ims/assets/images/favicon.ico') }}">
 
-        <meta charset="utf-8" />
-        <title>Dashboard | Inventory Management System</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="Themesdesign" name="author" />
-         <!-- Select 2 -->
-         <link href="{{ asset('ims/assets/libs/select2/css/select2.min.css') }}" rel="stylesheet" type="text/css">
-        <!-- end Select 2  -->
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="{{ asset('ims/assets/images/favicon.ico') }}">
+    <!-- jquery.vectormap css -->
+    <link href="{{ asset('ims/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}"
+        rel="stylesheet" type="text/css" />
 
-        <!-- jquery.vectormap css -->
-        <link href="{{ asset('ims/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}" rel="stylesheet" type="text/css" />
+    <!-- DataTables -->
+    <link href="{{ asset('ims/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet"
+        type="text/css" />
 
-        <!-- DataTables -->
-        <link href="{{ asset('ims/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Responsive datatable examples -->
+    <link href="{{ asset('ims/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}"
+        rel="stylesheet" type="text/css" />
 
-        <!-- Responsive datatable examples -->
-        <link href="{{ asset('ims/assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('ims/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet"
+        type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('ims/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('ims/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
-        <!-- Bootstrap Css -->
-        <link href="{{ asset('ims/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
-        <!-- Icons Css -->
-        <link href="{{ asset('ims/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-        <!-- App Css-->
-        <link href="{{ asset('ims/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+    {{-- Toaster cdn --}}
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css">
 
-        {{-- Toaster cdn --}}
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+</head>
 
-    </head>
-
-    <body data-topbar="dark">
+<body data-topbar="dark">
 
     <!-- <body data-layout="horizontal" data-topbar="dark"> -->
 
-        <!-- Begin page -->
-        <div id="layout-wrapper">
-            @include('admin.body.header')
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+        @include('admin.body.header')
 
-            <!-- ========== Left Sidebar Start ========== -->
-           @include('admin.body.sidebar')
-            <!-- Left Sidebar End -->
-
-
-
-            <!-- ============================================================== -->
-            <!-- Start right Content here -->
-            <!-- ============================================================== -->
-            <div class="main-content">
-                @yield('admin')
-                <!-- End Page-content -->
+        <!-- ========== Left Sidebar Start ========== -->
+        @include('admin.body.sidebar')
+        <!-- Left Sidebar End -->
 
 
-                @include('admin.body.footer')
 
-            </div>
-            <!-- end main content-->
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+            @yield('admin')
+            <!-- End Page-content -->
+
+
+            @include('admin.body.footer')
 
         </div>
-        <!-- END layout-wrapper -->
+        <!-- end main content-->
 
-        <!-- Right bar overlay-->
-        <div class="rightbar-overlay"></div>
+    </div>
+    <!-- END layout-wrapper -->
 
-        <!-- JAVASCRIPT -->
-        <script src="{{ asset('ims/assets/libs/jquery/jquery.min.js') }}"></script>
-        <script src="{{ asset('ims/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('ims/assets/libs/metismenu/metisMenu.min.js') }}"></script>
-        <script src="{{ asset('ims/assets/libs/simplebar/simplebar.min.js') }}"></script>
-        <script src="{{ asset('ims/assets/libs/node-waves/waves.min.js') }}"></script>
+    <!-- Right bar overlay-->
+    <div class="rightbar-overlay"></div>
+
+    <!-- JAVASCRIPT -->
+    <script src="{{ asset('ims/assets/libs/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('ims/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('ims/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('ims/assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('ims/assets/libs/node-waves/waves.min.js') }}"></script>
 
 
-        <!-- apexcharts -->
-        <script src="{{ asset('ims/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
+    <!-- apexcharts -->
+    <script src="{{ asset('ims/assets/libs/apexcharts/apexcharts.min.js') }}"></script>
 
-        <!-- jquery.vectormap map -->
-        <script src="{{ asset('ims/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-        <script src="{{ asset('ims/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js') }}"></script>
+    <!-- jquery.vectormap map -->
+    <script src="{{ asset('ims/assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+    <script src="{{ asset('ims/assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-us-merc-en.js') }}">
+    </script>
 
-        <!-- Required datatable js -->
-        <script src="{{ asset('ims/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-        <script src="{{ asset('ims/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <!-- Required datatable js -->
+    <script src="{{ asset('ims/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('ims/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
 
-        <!-- Responsive examples -->
-        <script src="{{ asset('ims/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
-        <script src="{{ asset('ims/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
+    <!-- Responsive examples -->
+    <script src="{{ asset('ims/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('ims/assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
 
-        <script src="{{ asset('ims/assets/js/pages/dashboard.init.js') }}"></script>
+    <script src="{{ asset('ims/assets/js/pages/dashboard.init.js') }}"></script>
 
-        <!-- App js -->
-        <script src="{{ asset('ims/assets/js/app.js') }}"></script>
+    <!-- App js -->
+    <script src="{{ asset('ims/assets/js/app.js') }}"></script>
 
-        {{-- toaster js --}}
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    {{-- toaster js --}}
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
-         <!-- Required datatable js -->
-        <script src="{{ asset('ims/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-        <script src="{{ asset('ims/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <!-- Required datatable js -->
+    <script src="{{ asset('ims/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('ims/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
 
-        <!-- Datatable init js -->
-        <script src="{{ asset('ims/assets/js/pages/datatables.init.js') }}"></script>
-        <script src="{{ asset('ims/assets/js/validate.min.js') }}"></script>
+    <!-- Datatable init js -->
+    <script src="{{ asset('ims/assets/js/pages/datatables.init.js') }}"></script>
+    <script src="{{ asset('ims/assets/js/validate.min.js') }}"></script>
 
-        <script src="{{ asset('ims/assets/js/validate.min.js') }}"></script>
-        <script src="{{ asset('ims/assets/js/handlebars.js') }}"></script>
+    <script src="{{ asset('ims/assets/js/validate.min.js') }}"></script>
+    <script src="{{ asset('ims/assets/js/handlebars.js') }}"></script>
 
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"\></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js" \></script>
 
-        <script src="{{ asset('ims/assets/js/code.js') }}"></script>
-         <!--  For Select2 -->
-        <script src="{{ asset('ims/assets/libs/select2/js/select2.min.js') }}"></script>
-        <script src="{{ asset('ims/assets/js/pages/form-advanced.init.js') }}"></script>
-          <!-- end  For Select2 -->
-        <script>
-            @if(Session::has('message'))
-            var type = "{{ Session::get('alert-type','info') }}"
-            switch(type){
-               case 'info':
-               toastr.info(" {{ Session::get('message') }} ");
-               break;
+    <script src="{{ asset('ims/assets/js/code.js') }}"></script>
+    <!--  For Select2 -->
+    <script src="{{ asset('ims/assets/libs/select2/js/select2.min.js') }}"></script>
+    <script src="{{ asset('ims/assets/js/pages/form-advanced.init.js') }}"></script>
+    <!-- end  For Select2 -->
+    <script>
+        @if (Session::has('message'))
+            var type = "{{ Session::get('alert-type', 'info') }}"
+            switch (type) {
+                case 'info':
+                    toastr.info(" {{ Session::get('message') }} ");
+                    break;
 
-               case 'success':
-               toastr.success(" {{ Session::get('message') }} ");
-               break;
+                case 'success':
+                    toastr.success(" {{ Session::get('message') }} ");
+                    break;
 
-               case 'warning':
-               toastr.warning(" {{ Session::get('message') }} ");
-               break;
+                case 'warning':
+                    toastr.warning(" {{ Session::get('message') }} ");
+                    break;
 
-               case 'error':
-               toastr.error(" {{ Session::get('message') }} ");
-               break;
+                case 'error':
+                    toastr.error(" {{ Session::get('message') }} ");
+                    break;
             }
-            @endif
-           </script>
-    </body>
+        @endif
+    </script>
+</body>
 
 </html>
